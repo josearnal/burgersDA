1db88d65cf131ea40ee21496eb886b426e6ab020
Jose <jose.arnal@mail.utoronto.ca>
2023-10-12 06:29:11 -0400

    added flux calculation and input parameters (untested)

--- burgersDA/burgersDA.py
+++ burgersDA/burgersDA.py
@@ -4,10 +4,11 @@ class Cell:
 
     def __init__(self):
         self.u = 0.0 # solution variable
-        self.f = 0.0 # x flux at East face
-        self.g = 0.0 # y flux at South face
-        self.h = 0.0 # z flux at Bottom face
+        # self.f = 0.0 # x flux at East face
+        # self.g = 0.0 # y flux at South face
+        # self.h = 0.0 # z flux at Bottom face
         self.dudX = np.zeros(3) # solution gradient
+        self.dudt = 0.0 # solution rate of change
         self.phi = 0.0 # Limiter
         self.X = np.zeros(3) # Cell centroid
         self.dX = np.zeros(3) # Dimensions of cell
@@ -47,8 +48,11 @@ class Cell:
 
 class Block:
     
-    def __init__(self,L,M):
-        self.verify_input(L,M)
+    def __init__(self,IPs):
+        self.verify_input(IPs)
+        L = IPs["Block Dimensions"]
+        M = IPs["Number of Cells"]
+        self.order = IPs["Reconstruction Order"]
 
         self.NGc = 4 # Number of ghost cells, 4 extra cells (2 on each side) in each direction
         self.L = L.astype(np.float64) # Physical dimensions of block
@@ -90,15 +94,22 @@ class Block:
            raise Exception("Initial condition not yet implemented")
        
     @staticmethod
-    def verify_input(L,M):
+    def verify_input(IPs):
+        L = IPs["Block Dimensions"]
+        M = IPs["Number of Cells"]
+        order = IPs["Reconstruction Order"]
+
         if not isinstance(L, np.ndarray) or L.size != 3:
-         raise ValueError("Block: L array is not of the right shape")
+            raise ValueError("Block: L array is not of the right shape")
         
         if not isinstance(M, np.ndarray) or M.size != 3:
-         raise ValueError("Block: M array is not of the right shape")
+            raise ValueError("Block: M array is not of the right shape")
         
         if not np.issubdtype(M.dtype, np.integer):
-           raise TypeError("Block: M array is not integer array")
+            raise TypeError("Block: M array is not integer array")
+
+        if order != type(int()) and x < 1:
+            raise ValueError("Block: order must be positive integer")
         
     def Gaussian_Bump(self):
        
@@ -112,6 +123,64 @@ class Block:
                     else:
                         self.grid[i][j][k].u = 0.0
                        
+    def evaluate_residual(self):
+        if (self.order == 1):
+            self.clear_old_residual()
+            self.apply_BCs()
+            self.fluxes_order1()
+        else:
+            raise NotImplementedError("residual evaluation not implemented for this order")
+
+    def fluxes_order1(self):
+        
+        Ngc = self.NGc/2
+        # Loop from 1st inner cell to last inner cell in x
+        #           1st inner cell to last inner cell in y
+        #           1st inner cell to last inner cell in z.
+        for i in range(Ngc, self.M[0] - Ngc_east): 
+            for j in range(Ngc, self.M[1] - Ngc_east):
+                for k in range(Ngc, self.M[2] - Ngc_east):
+                    v = self.grid[i][j][k].volume
+                    eA = self.grid[i][j][k].eastArea
+                    sA = self.grid[i][j][k].southArea
+                    bA = self.grid[i][j][k].bottomArea
+
+                    wA = self.grid[i][j][k].westArea
+                    nA = self.grid[i][j][k].northArea
+                    tA = self.grid[i][j][k].topArea
+
+                    ul = self.grid[i-1][j][k].u
+                    ur = self.grid[i][j][k].u
+
+                    us = self.grid[i][j-1][k].u
+                    un = self.grid[i][j][k].u
+
+                    ub = self.grid[i][j][k-1].u
+                    ut = self.grid[i][j][k].u
+
+                    f = self.grid[i][j][k].RiemannFlux(ul,ur)
+                    g = self.grid[i][j][k].RiemannFlux(us,un)
+                    h = self.grid[i][j][k].RiemannFlux(ub,ut)
+
+                    self.grid[i][j][k].dudt += (eA/v)*f
+                    self.grid[i][j][k].dudt += (sA/v)*g
+                    self.grid[i][j][k].dudt += (bA/v)*h
+
+                    v = self.grid[i+1][j][k].volume
+                    self.grid[i+1][j][k].dudt -= (wA/v)*f
+
+                    v = self.grid[i][j+1][k].volume
+                    self.grid[i][j+1][k].dudt -= (nA/v)*g
+
+                    v = self.grid[i][j][k+1].volume
+                    self.grid[i][j][k+1].dudt -= (tA/v)*h
 
 
+
+# class InputParameters:
+
+#     def __init__(self,dictionary):
+#         self.L = dictionary["Block Dimensions"]
+#         self.M = dictionary["Number of Cells"]
+#         self.order = dictionary["Reconstruction Order"]
 
